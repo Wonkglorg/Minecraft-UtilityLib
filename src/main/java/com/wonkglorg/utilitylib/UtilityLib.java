8b73e9a42fceedc820afaf352073afe002e9d517
Wonkglorg <Wonkglorg.18@gmail.com>
2022-07-26 21:10:00 +0200

    Beta Release

--- src/main/java/com/wonkglorg/utilitylib/UtilityLib.java
+++ src/main/java/com/wonkglorg/utilitylib/UtilityLib.java
@@ -1,7 +1,5 @@
 package com.wonkglorg.utilitylib;
 
-import com.wonkglorg.utilitylib.test.JoinListener;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UtilityLib extends JavaPlugin
@@ -9,8 +7,7 @@ public final class UtilityLib extends JavaPlugin
 	@Override
 	public void onEnable()
 	{
-		PluginManager manager = getServer().getPluginManager();
-		manager.registerEvents(new JoinListener(),this);
+	
 	}
 	
 	@Override
