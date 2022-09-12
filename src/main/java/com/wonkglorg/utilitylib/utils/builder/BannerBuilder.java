8097a3edfa3f83e5dedb7256bea60b6d354e4215
Wonkglorg <Wonkglorg.18@gmail.com>
2022-09-12 08:03:00 +0200

    Beta Release

--- src/main/java/com/wonkglorg/utilitylib/utils/builder/BannerBuilder.java
+++ src/main/java/com/wonkglorg/utilitylib/utils/builder/BannerBuilder.java
@@ -14,12 +14,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The type Banner builder.
+ */
 @SuppressWarnings("unused")
 public class BannerBuilder extends ItemBuilder
 {
-	private List<DyeColor> colorList;
-	private List<PatternType> patternList;
 	
+	private List<BannerLayer> bannerLayerList;
+	
+	/**
+	 * Instantiates a new Banner builder.
+	 *
+	 * @param color the color
+	 * @param amount the amount
+	 * @param name the name
+	 * @param lore the lore
+	 * @param durability the durability
+	 * @param meta the meta
+	 * @param flags the flags
+	 * @param enchantments the enchantments
+	 */
 	public BannerBuilder(DyeColor color,
 						 int amount,
 						 String name,
@@ -32,78 +47,138 @@ public class BannerBuilder extends ItemBuilder
 		super(Material.valueOf(color.name() + "_BANNER"), amount, name, lore, durability, meta, flags, enchantments);
 	}
 	
+	/**
+	 * Instantiates a new Banner builder.
+	 *
+	 * @param color the color
+	 */
 	public BannerBuilder(DyeColor color)
 	{
 		super(Material.valueOf(color.name() + "_BANNER"));
 	}
 	
+	/**
+	 * Instantiates a new Banner builder.
+	 *
+	 * @param color the color
+	 * @param itemFlags the item flags
+	 */
 	public BannerBuilder(DyeColor color, ItemFlag... itemFlags)
 	{
 		super(Material.valueOf(color.name() + "_BANNER"), itemFlags);
 	}
 	
+	/**
+	 * Instantiates a new Banner builder.
+	 *
+	 * @param color the color
+	 * @param name the name
+	 */
 	public BannerBuilder(DyeColor color, String name)
 	{
 		super(Material.valueOf(color.name() + "_BANNER"), name);
 	}
 	
+	/**
+	 * Instantiates a new Banner builder.
+	 *
+	 * @param color the color
+	 * @param description the description
+	 */
 	public BannerBuilder(DyeColor color, List<String> description)
 	{
 		super(Material.valueOf(color.name() + "_BANNER"), description);
 	}
 	
+	/**
+	 * Instantiates a new Banner builder.
+	 *
+	 * @param color the color
+	 * @param amount the amount
+	 */
 	public BannerBuilder(DyeColor color, int amount)
 	{
 		super(Material.valueOf(color.name() + "_BANNER"), amount);
 	}
 	
+	/**
+	 * Instantiates a new Banner builder.
+	 *
+	 * @param color the color
+	 * @param amount the amount
+	 * @param name the name
+	 */
 	public BannerBuilder(DyeColor color, int amount, String name)
 	{
 		super(Material.valueOf(color.name() + "_BANNER"), amount, name);
 	}
 	
+	/**
+	 * Instantiates a new Banner builder.
+	 *
+	 * @param color the color
+	 * @param amount the amount
+	 * @param name the name
+	 * @param description the description
+	 */
 	public BannerBuilder(DyeColor color, int amount, String name, List<String> description)
 	{
 		super(Material.valueOf(color.name() + "_BANNER"), amount, name, description);
 	}
 	
+	/**
+	 * Add layer banner builder.
+	 *
+	 * @param color the color
+	 * @param patternType the pattern type
+	 * @return the banner builder
+	 */
 	public BannerBuilder addLayer(DyeColor color, PatternType patternType)
 	{
-		if(colorList == null)
+		if(bannerLayerList == null)
 		{
-			colorList = new ArrayList<>();
+			bannerLayerList = new ArrayList<>();
 		}
-		if(patternList == null)
-		{
-			patternList = new ArrayList<>();
-		}
-		colorList.add(color);
-		patternList.add(patternType);
+		bannerLayerList.add(new BannerLayer(color, patternType));
 		return this;
 	}
 	
+	/**
+	 * Replace color at banner builder.
+	 *
+	 * @param index the index
+	 * @param color the color
+	 * @return the banner builder
+	 */
 	public BannerBuilder replaceColorAt(int index, DyeColor color)
 	{
-		if(colorList == null)
+		if(bannerLayerList == null)
 		{
-			colorList = new ArrayList<>();
+			bannerLayerList = new ArrayList<>();
 		}
-		if(index >= colorList.size())
+		if(index < bannerLayerList.size())
 		{
-			colorList.set(index, color);
+			bannerLayerList.set(index, new BannerLayer(color, bannerLayerList.get(index).getPatternType()));
 		}
 		return this;
 	}
 	
+	/**
+	 * Replace pattern at banner builder.
+	 *
+	 * @param index the index
+	 * @param type the type
+	 * @return the banner builder
+	 */
 	public BannerBuilder replacePatternAt(int index, PatternType type)
 	{
-		if(patternList == null)
+		if(bannerLayerList == null)
 		{
-			patternList = new ArrayList<>();
+			bannerLayerList = new ArrayList<>();
 		}
-		if(index >= patternList.size())
+		if(index < bannerLayerList.size())
 		{
-			patternList.set(index, type);
+			bannerLayerList.set(index, new BannerLayer(bannerLayerList.get(index).getDyeColor(), type));
 		}
 		return this;
 	}
@@ -112,15 +187,124 @@ public class BannerBuilder extends ItemBuilder
 	public ItemStack build()
 	{
 		List<Pattern> patterns = new ArrayList<>();
-		for(int i = 0; i < patternList.size(); i++)
+		for(BannerLayer bannerLayer : bannerLayerList)
 		{
-			patterns.add(new Pattern(colorList.get(i), patternList.get(i)));
+			patterns.add(new Pattern(bannerLayer.getDyeColor(), bannerLayer.getPatternType()));
 		}
 		super.setItem(new ItemStack(getMaterial(), getAmount()));
 		BannerMeta meta = (BannerMeta) super.getItem().getItemMeta();
 		meta.setPatterns(patterns);
 		super.setMeta(meta);
 		return super.build();
+	}
+	
+	/**
+	 * Removes last pattern layer
+	 *
+	 * @return instance of banner builder
+	 */
+	public BannerBuilder removeLastLayer()
+	{
+		if(bannerLayerList == null)
+		{
+			bannerLayerList = new ArrayList<>();
+		}
+		bannerLayerList.remove(bannerLayerList.size() - 1);
+		return this;
+	}
+	
+	/**
+	 * Removes first pattern layer
+	 *
+	 * @return instance of banner builder
+	 */
+	public BannerBuilder removeFirstLayer()
+	{
+		
+		if(bannerLayerList == null)
+		{
+			bannerLayerList = new ArrayList<>();
+		}
+		bannerLayerList.remove(0);
+		return this;
+	}
+	
+	/**
+	 * Removes banner layer at the selected index
+	 *
+	 * @param index the index
+	 * @return instance of banner builder
+	 */
+	public BannerBuilder removeLayerAt(int index)
+	{
+		
+		if(bannerLayerList == null)
+		{
+			bannerLayerList = new ArrayList<>();
+		}
+		if(index < bannerLayerList.size())
+		{
+			bannerLayerList.remove(index);
+		}
+		return this;
+	}
+	
+	private static class BannerLayer
+	{
+		private DyeColor dyeColor;
+		private PatternType patternType;
+		
+		/**
+		 * Instantiates a new Banner layer.
+		 *
+		 * @param dyeColor the dye color
+		 * @param patternType the pattern type
+		 */
+		public BannerLayer(DyeColor dyeColor, PatternType patternType)
+		{
+			this.dyeColor = dyeColor;
+			this.patternType = patternType;
+		}
+		
+		/**
+		 * Gets dye color.
+		 *
+		 * @return the dye color
+		 */
+		public DyeColor getDyeColor()
+		{
+			return dyeColor;
+		}
+		
+		/**
+		 * Sets dye color.
+		 *
+		 * @param dyeColor the dye color
+		 */
+		public void setDyeColor(DyeColor dyeColor)
+		{
+			this.dyeColor = dyeColor;
+		}
+		
+		/**
+		 * Gets pattern type.
+		 *
+		 * @return the pattern type
+		 */
+		public PatternType getPatternType()
+		{
+			return patternType;
+		}
+		
+		/**
+		 * Sets pattern type.
+		 *
+		 * @param patternType the pattern type
+		 */
+		public void setPatternType(PatternType patternType)
+		{
+			this.patternType = patternType;
+		}
 	}
 	
 }
