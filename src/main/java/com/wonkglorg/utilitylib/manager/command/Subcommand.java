670f327975824850f8f0d03a1d7220ccfcc3f644
Wonkglorg <Wonkglorg.18@gmail.com>
2023-06-18 09:25:44 +0200

    -Added String varient to ItemBuilder

--- src/main/java/com/wonkglorg/utilitylib/manager/command/Subcommand.java
+++ src/main/java/com/wonkglorg/utilitylib/manager/command/Subcommand.java
@@ -2,9 +2,21 @@ package com.wonkglorg.utilitylib.manager.command;
 
 import org.bukkit.command.CommandSender;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public abstract class Subcommand extends Arguments{
+	protected String[] subCommandNames;
+	
+	public Subcommand(String... subCommandName) {
+		this.subCommandNames = subCommandName;
+	}
+	
+	public boolean matches(String arg) {
+		return Arrays.stream(subCommandNames).anyMatch(Predicate.isEqual(arg));
+	}
+	
 	public abstract void execute(CommandSender sender, String[] args);
 	
 	public abstract List<String> tabComplete(String alias, String[] args);
